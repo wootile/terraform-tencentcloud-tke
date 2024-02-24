74571f701e928dae77fe2c36010852eb23a3620e
Ming Chen <netcmcc@outlook.com>
2024-02-24 14:36:54 +0800

    Change version requirements

--- examples/complete/versions.tf
+++ examples/complete/versions.tf
@@ -2,11 +2,11 @@
 
 ### Define terraform version and required providers
 terraform {
-  required_version = ">= 1.3.6"
+  required_version = ">= 1.1.2"
   required_providers {
     tencentcloud = {
       source  = "tencentcloudstack/tencentcloud"
-      version = ">= 1.81"
+      version = ">= 1.67.0"
     }
   }
 }
